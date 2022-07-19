7c794bfb8d05c6a94234cfd65291cd69c03ea760
JavaKira <JavaKira@yandex.ru>
2022-07-19 12:09:51 +0300

    Fix: fixes MainMenu.fxml layout

--- src/main/java/com/example/mlauncher/HelloApplication.java
+++ src/main/java/com/example/mlauncher/HelloApplication.java
@@ -10,9 +10,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MainMenu.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
+        stage.setWidth(600);
+        stage.setHeight(400);
         stage.setScene(scene);
         stage.show();
     }
