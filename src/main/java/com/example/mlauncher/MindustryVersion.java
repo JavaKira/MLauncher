9c43c261ce28a4616aefeddef66c9d674ec78289
JavaKira <JavaKira@yandex.ru>
2022-07-22 23:19:30 +0300

    Feature: adds MindustryVersion.java isDownloaded()

--- src/main/java/com/example/mlauncher/MindustryVersion.java
+++ src/main/java/com/example/mlauncher/MindustryVersion.java
@@ -1,5 +1,7 @@
 package com.example.mlauncher;
 
+import java.io.File;
+
 public class MindustryVersion {
     private final String name;
 
@@ -10,5 +12,9 @@ public class MindustryVersion {
     @Override
     public String toString() {
         return name;
+    }
+
+    public boolean isDownloaded() {
+        return new File(System.getenv("APPDATA") + "\\MLauncher\\Versions\\" + name).exists();
     }
 }
