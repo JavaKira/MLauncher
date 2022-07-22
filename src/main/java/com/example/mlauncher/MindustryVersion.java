b14696cb60b4bafe5e359cf8cdc70988ca2c5c56
JavaKira <JavaKira@yandex.ru>
2022-07-22 13:05:07 +0300

    Feature: adds MindustryVersion.java and MindustryVersionPool.java

---
+++ src/main/java/com/example/mlauncher/MindustryVersion.java
@@ -0,0 +1,14 @@
+package com.example.mlauncher;
+
+public class MindustryVersion {
+    private final String name;
+
+    public MindustryVersion(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
---
+++ src/main/java/com/example/mlauncher/MindustryVersionPool.java
@@ -0,0 +1,25 @@
+package com.example.mlauncher;
+
+import com.example.mlauncher.util.URLJsonReader;
+
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MindustryVersionPool {
+    private final List<MindustryVersion> objects = new ArrayList<>();
+
+    public void initialize() {
+        JsonArray arr = URLJsonReader.readJsonArray("https://api.github.com/repos/anuken/Mindustry/releases");
+        for (int i = 0; i < arr.size(); i++) {
+            JsonObject obj = arr.getJsonObject(i);
+            objects.add(new MindustryVersion(obj.getString("name")));
+        }
+    }
+
+    public List<MindustryVersion> getObjects() {
+        return Collections.unmodifiableList(objects);
+    }
+}
