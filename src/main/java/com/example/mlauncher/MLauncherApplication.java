27304d5ab77a6cf8ea9452774c6b0922d40c2e3a
JavaKira <JavaKira@yandex.ru>
2022-07-25 13:35:58 +0300

    Style: renames HelloApplication.java to MLauncherApplication.java

--- src/main/java/com/example/mlauncher/MLauncherApplication.java
+++ src/main/java/com/example/mlauncher/MLauncherApplication.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MLauncherApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MainMenu.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MLauncherApplication.class.getResource("MainMenu.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("MLauncher");
         stage.setWidth(600);
         stage.setHeight(400);
         stage.setScene(scene);
