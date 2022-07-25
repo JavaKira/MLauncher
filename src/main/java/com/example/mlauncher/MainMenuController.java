535458b2f0aa94aa4849e4ed2b055ff320cecadf
JavaKira <JavaKira@yandex.ru>
2022-07-25 21:13:37 +0300

    Feature: adds simple MainMenuController.java

---
+++ src/main/java/com/example/mlauncher/MainMenuController.java
@@ -0,0 +1,36 @@
+package com.example.mlauncher;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ProgressBar;
+
+public class MainMenuController {
+    private final MindustryVersionPool mindustryVersionPool;
+
+    @FXML
+    private ChoiceBox<MindustryVersion> versionBox;
+    @FXML
+    private ProgressBar downloadProgressBar;
+    @FXML
+    private Button actionButton;
+
+    public MainMenuController() {
+        mindustryVersionPool = new MindustryVersionPool();
+        mindustryVersionPool.initialize();
+    }
+
+    @FXML
+    public void initialize() {
+        versionBox.getItems().addAll(mindustryVersionPool.getObjects());
+        versionBox.onActionProperty().setValue(event -> {
+            actionButton.setText(versionBox.getValue().isDownloaded() ? "Launch" : "Download");
+            actionButton.onActionProperty().setValue(event1 -> {
+                if (versionBox.getValue().isDownloaded())
+                    versionBox.getValue().launch();
+                else
+                    versionBox.getValue().download();
+            });
+        });
+    }
+}
