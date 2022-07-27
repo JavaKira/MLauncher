0a9a2c12d68fc08ef3fa126cc8aae3ded762d493
JavaKira <JavaKira@yandex.ru>
2022-07-27 23:04:15 +0300

    Feature: adds icon

--- src/main/java/com/example/mlauncher/MLauncherApplication.java
+++ src/main/java/com/example/mlauncher/MLauncherApplication.java
@@ -3,9 +3,12 @@ package com.example.mlauncher;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
 
 public class MLauncherApplication extends Application {
     @Override
@@ -13,6 +16,9 @@ public class MLauncherApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(MLauncherApplication.class.getResource("MainMenu.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("MLauncher");
+        List.of(16, 32, 64, 128, 256, 512).forEach(integer -> {
+            stage.getIcons().add(new Image(Objects.requireNonNull(MLauncherApplication.class.getResourceAsStream("icons/icon" + integer + ".png"))));
+        });
         stage.setWidth(600);
         stage.setHeight(400);
         stage.setScene(scene);
