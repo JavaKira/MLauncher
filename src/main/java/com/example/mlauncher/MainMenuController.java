bd860f1de7958eec68848cdedc4a9cc02c71ef28
JavaKira <JavaKira@yandex.ru>
2022-07-28 13:02:42 +0300

    Feature: adds update MainMenu actionButton on download ends

--- src/main/java/com/example/mlauncher/MainMenuController.java
+++ src/main/java/com/example/mlauncher/MainMenuController.java
@@ -1,6 +1,7 @@
 package com.example.mlauncher;
 
 import com.example.mlauncher.util.FileDownload;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
@@ -25,7 +26,7 @@ public class MainMenuController {
     public void initialize() {
         versionBox.getItems().addAll(mindustryVersionPool.getObjects());
         versionBox.onActionProperty().setValue(event -> {
-            actionButton.setText(versionBox.getValue().isDownloaded() ? "Launch" : "Download");
+            updateActionButton();
             actionButton.onActionProperty().setValue(event1 -> {
                 if (versionBox.getValue().isDownloaded())
                     versionBox.getValue().launch();
@@ -33,9 +34,16 @@ public class MainMenuController {
                     downloadProgressBar.setOpacity(1);
                     FileDownload fileDownload = versionBox.getValue().download();
                     fileDownload.setOnRead(integer -> downloadProgressBar.setProgress((float)integer / versionBox.getValue().getSize()));
-                    fileDownload.setOnEnd(event2 -> downloadProgressBar.setOpacity(0));
+                    fileDownload.setOnEnd(event2 -> {
+                        downloadProgressBar.setOpacity(0);
+                        Platform.runLater(this::updateActionButton);
+                    });
                 }
             });
         });
+    }
+
+    public void updateActionButton() {
+        actionButton.setText(versionBox.getValue().isDownloaded() ? "Launch" : "Download");
     }
 }
