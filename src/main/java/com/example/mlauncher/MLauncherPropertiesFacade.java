56c4bdeee35540e614d0d60ec17d1377b8594ad4
JavaKira <JavaKira@yandex.ru>
2022-08-25 13:50:37 +0300

    Feature: adds MLauncherPropertiesFacade.java properties get/set methods and SettingsPage functional

--- src/main/java/com/example/mlauncher/MLauncherPropertiesFacade.java
+++ src/main/java/com/example/mlauncher/MLauncherPropertiesFacade.java
@@ -20,7 +20,13 @@ public class MLauncherPropertiesFacade {
         file = new File("settings.properties");
         properties = new Properties();
         try {
-            if (!file.exists()) file.createNewFile();
+            if (!file.exists()) {
+                file.createNewFile();
+                setDefaults();
+                storeProperties();
+                return;
+            }
+
             properties.load(new FileInputStream(file));
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -33,6 +39,45 @@ public class MLauncherPropertiesFacade {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void setDefaults() {
+        setBeBuilds(false);
+        setOldBeBuilds(false);
+        setOldVersions(true);
+        setPath(System.getenv("APPDATA") + "\\MLauncher\\Versions\\");
+    }
+
+    public void setBeBuilds(boolean beBuilds) {
+        properties.setProperty("beBuilds", String.valueOf(beBuilds));
+    }
+
+    public boolean getBeBuilds() {
+        return Boolean.parseBoolean(properties.getProperty("beBuilds"));
+    }
+
+    public void setOldBeBuilds(boolean oldBeBuilds) {
+        properties.setProperty("oldBeBuilds", String.valueOf(oldBeBuilds));
+    }
+
+    public boolean getOldBeBuilds() {
+        return Boolean.parseBoolean(properties.getProperty("oldBeBuilds"));
+    }
+
+    public void setOldVersions(boolean oldVersions) {
+        properties.setProperty("oldVersions", String.valueOf(oldVersions));
+    }
+
+    public boolean getOldVersions() {
+        return Boolean.parseBoolean(properties.getProperty("oldVersions"));
+    }
+
+    public void setPath(String path) {
+        properties.setProperty("path", path);
+    }
+
+    public String getPath() {
+        return properties.getProperty("path");
     }
 
     public Properties getProperties() {
--- src/main/java/com/example/mlauncher/controller/SettingsPageController.java
+++ src/main/java/com/example/mlauncher/controller/SettingsPageController.java
@@ -1,13 +1,65 @@
 package com.example.mlauncher.controller;
 
+import com.example.mlauncher.MLauncherPropertiesFacade;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class SettingsPageController implements Initializable {
+    @FXML
+    private CheckBox beBuildsCheckBox;
+    @FXML
+    private CheckBox oldBeBuildCheckBox;
+    @FXML
+    private CheckBox oldVersionsCheckBox;
+    @FXML
+    private TextField pathTextField;
+    @FXML
+    private Button pathOpenButton;
+    @FXML
+    private Button resetButton;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        beBuildsCheckBox.setSelected(MLauncherPropertiesFacade.getInstance().getBeBuilds());
+        beBuildsCheckBox.setOnAction(actionEvent -> {
+            MLauncherPropertiesFacade.getInstance().setBeBuilds(beBuildsCheckBox.isSelected());
+            MLauncherPropertiesFacade.getInstance().storeProperties();
+        });
+        oldBeBuildCheckBox.setSelected(MLauncherPropertiesFacade.getInstance().getOldBeBuilds());
+        oldBeBuildCheckBox.setOnAction(actionEvent -> {
+            MLauncherPropertiesFacade.getInstance().setOldBeBuilds(oldBeBuildCheckBox.isSelected());
+            MLauncherPropertiesFacade.getInstance().storeProperties();
+        });
+        oldVersionsCheckBox.setSelected(MLauncherPropertiesFacade.getInstance().getOldVersions());
+        oldVersionsCheckBox.setOnAction(actionEvent -> {
+            MLauncherPropertiesFacade.getInstance().setOldVersions(oldVersionsCheckBox.isSelected());
+            MLauncherPropertiesFacade.getInstance().storeProperties();
+        });
+        pathTextField.setText(MLauncherPropertiesFacade.getInstance().getPath());
+        pathTextField.setOnAction(actionEvent -> {
+            MLauncherPropertiesFacade.getInstance().setPath(pathTextField.getText());
+            MLauncherPropertiesFacade.getInstance().storeProperties();
+        });
+        pathOpenButton.setOnAction(actionEvent -> {
+            try {
+                Runtime.getRuntime().exec(new String[]{"explorer", '"' + pathTextField.getText() + '"'});
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        resetButton.setOnAction(actionEvent -> {
+            MLauncherPropertiesFacade.getInstance().setDefaults();
+            MLauncherPropertiesFacade.getInstance().storeProperties();
+            initialize(location, resources);
+        });
     }
 }
