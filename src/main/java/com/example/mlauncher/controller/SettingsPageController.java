03113350e6e977975cb2b15b776599e9c5d7d71f
JavaKira <JavaKira@yandex.ru>
2022-10-22 11:06:25 +0300

    Feature: adds versionPool in SettingsPageController, that allow set max version for sliders

--- src/main/java/com/example/mlauncher/controller/SettingsPageController.java
+++ src/main/java/com/example/mlauncher/controller/SettingsPageController.java
@@ -1,6 +1,8 @@
 package com.example.mlauncher.controller;
 
 import com.example.mlauncher.PropertiesFacade;
+import com.example.mlauncher.Version;
+import com.example.mlauncher.VersionPool;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -9,7 +11,9 @@ import javafx.scene.control.TextField;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class SettingsPageController implements Initializable {
     @FXML
@@ -23,14 +27,25 @@ public class SettingsPageController implements Initializable {
     @FXML
     private Button resetButton;
 
+    private final VersionPool versionPool;
+
+    public SettingsPageController() {
+        versionPool = new VersionPool();
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        List<Version> beBuilds = versionPool.getObjects().stream().filter(Version::isBE).collect(Collectors.toList());
+        List<Version> builds = versionPool.getObjects().stream().filter(version -> !version.isBE()).collect(Collectors.toList());
+
+        beBuildsSlider.setMax(beBuilds.size());
         beBuildsSlider.setValue(PropertiesFacade.getInstance().getBeBuilds());
         beBuildsSlider.setOnMouseReleased(actionEvent -> {
             System.out.println(beBuildsSlider.getValue());
             PropertiesFacade.getInstance().setBeBuilds((int) beBuildsSlider.getValue());
             PropertiesFacade.getInstance().storeProperties();
         });
+        buildsSlider.setMax(builds.size());
         buildsSlider.setValue(PropertiesFacade.getInstance().getVersions());
         buildsSlider.setOnMouseReleased(actionEvent -> {
             PropertiesFacade.getInstance().setVersions((int) buildsSlider.getValue());
